e31d50255b02e8186000720a76ba0c7840355d69
Alexandr <booharin@bk.ru>
2020-07-13 22:05:33 +0300

    Added project

---
+++ lerua/spiders/leroymerlin.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class LeroymerlinSpider(scrapy.Spider):
+    name = 'leroymerlin'
+    allowed_domains = ['leroymerlin.ru']
+
+    def __init__(self, search):
+        self.start_urls = [f'https://leroymerlin.ru/search/?q={search}']
+
+    def parse(self, response):
+        pass
